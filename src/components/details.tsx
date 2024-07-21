a236978a79b008329012d54f7f9fbf5cc78ffea4
leonel <leonel@DESKTOP-0SDEIO0>
2024-07-21 19:31:21 -0300

    chore: remove warning due to unoptimized images
    Animated images cannot be optimized by NextJS

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -9,7 +9,7 @@ interface IDetailsProps {
 export const Details = ({ image, alt, text }: Readonly<IDetailsProps>) => {
   return (
     <div className="grid grid-row-2 gap-4 justify-items-center md:grid-row-0 md:grid-cols-2 md:gap-12 md:w-[800px] md:items-center border border-4 border-black p-6 bg-red-500">
-      <Image src={image} alt={alt} />
+      <Image src={image} alt={alt} unoptimized />
       <p className="text-white">{text}</p>
     </div>
   );
